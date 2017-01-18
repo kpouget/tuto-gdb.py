6a21c3ccafa9d8dab509838fb58dddb3e58d702e
Kevin Pouget <kevin.pouget@imag.fr>
2017-01-18 13:50:42 +0100

    simplify docker commandline

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,24 @@
 FROM finalduty/archlinux:monthly
-#FROM kpouget/tuto-gdb.py
+
 MAINTAINER Kevin Pouget <kpouget@imag.fr>
 
 ENTRYPOINT ["bash"]
 CMD ["init.sh"]
 
-RUN pacman --noconfirm --needed -Sy gdb strace libdwarf make binutils gcc vim \
+RUN pacman --noconfirm --needed -Sy gdb strace libdwarf make binutils gcc vim sudo \
     && rm -rf /var/cache/pacman/pkg/ \
     && mkdir -p /var/cache/pacman/pkg/
 
+RUN echo "## Allow all users to run any commands anywhere" >> /etc/sudoers
+RUN echo "ALL ALL=(ALL) 	ALL" >> /etc/sudoers
+RUN echo "auth    sufficient    pam_permit.so" > /etc/pam.d/su
 
 RUN mkdir -p /home/gdb
 WORKDIR /home/gdb
 
 COPY home /home/gdb
-COPY Dockerfile /home/gdb
 
 RUN date > /home/gdb/.version
+COPY Dockerfile /home/gdb
+
 VOLUME /home/gdb
--- home/init.sh
+++ home/init.sh
@@ -3,53 +3,72 @@
 cat <<EOF
 ##########
 #
-# Understanding, Scripting and Extending GDB
+# Understanding, Scripting and Extending GDB, interactive part
 #
 ##########
-#
-##########
-# Running an Archlinux container
-##########
 EOF
+##########
 
-##### check env #####
 
-if [ -z "$USERID" ]
-then
-  echo "FATAL: USERID/GROUPID environment variables not set. Did you pass it to docker?" 
-  echo "       Please run docker as follows:"
-  cat <<EOF
+##### ENVIRONMENT #####
 
-HOST_DIR=\$HOME/gdb_data # create it first !
-docker run -it -v \$HOST_DIR:/home/gdb/host -e GROUPID=\$(id -g) -e USERID=\$(id -u) --cap-add sys_ptrace kpouget/tuto-gdb.py
-EOF
-   exec bash
-fi
+HOST_MNT=/home/gdb/host
+USER_NAME=gdb
+IMAGE_NAME=kpouget/tuto-gdb.py
 
 ##### admin stuff #####
 
-echo "root:root" | chpasswd && echo "INFO: root password set to 'root'."
-groupadd --gid $GROUPID gdb --non-unique
-useradd --uid $USERID --gid $GROUPID gdb
-chown gdb:gdb /home/gdb/ -R
-echo "INFO: Docker image version $(cat /home/gdb/.version)"
+
+echo "##########"
+echo "# Running an Archlinux container"
+echo "# Docker image built on $(cat /home/$USER_NAME/.version)"
+echo "##########"
+echo
+
+GROUPID=$(stat $HOST_MNT -c %g)
+USERID=$(stat $HOST_MNT -c %u)
+
+if [[ $USERID -eq 0 && $GROUPID -eq 0 ]]
+then
+
+    echo "####################"
+    echo "WARNING: Directory '$HOST_MNT' not shared with host, is it on purpose ? if not, please use this command line:"
+    echo "    HOST_DIR=\$HOME/${USER_NAME}_data; mkdir -p \$HOST_DIR"
+    echo "    docker run --rm -it -v \$HOST_DIR:/home/$USER_NAME/host  --cap-add sys_ptrace $IMAGE_NAME"
+    echo "####################"
+    echo
+    GROUPID=1000
+    USERID=1000
+    NO_HOST=1
+fi
+
+echo "root:root" | chpasswd 
+
+groupadd --gid $GROUPID $USER_NAME --non-unique
+useradd --uid $USERID --gid $GROUPID $USER_NAME
+
+chown $USER_NAME:$USER_NAME /home/$USER_NAME/ -R
+
 ##### check strace #####
 
 strace ls &>/dev/null
 if [ $? -eq 1 ] 
 then
+    echo "####################"
     echo 'ERROR: ptrace not working. Did you pass --privileged or --cap-add sys_ptrace option to docker ?'
+    echo "####################"
+    echo
 fi
 
 ##### prepare host #####
 
-HOST_MNT=/home/gdb/host
-if [ -z "$(ls -A $HOST_MNT/dwarf $HOST_MNT/python 2>/dev/null)" ] 
+if [[ -z "$NO_HOST" && -z "$(ls -A $HOST_MNT/dwarf $HOST_MNT/python 2>/dev/null)" ]]
 then
     echo "INFO: Running './prepare_host.sh' to populate $HOST_MNT host-shared directory."
-    su gdb -c ./prepare_host.sh
+    su  $USER_NAME -c ./prepare_host.sh
 fi
 
-echo "INFO: Read ~/README or ~/presentation.pdf for usage details"
+echo "INFO: Read ~/README, ~/presentation.pdf and ~/exercices.md for usage details"
+echo
 
-exec su gdb
+exec su $USER_NAME
